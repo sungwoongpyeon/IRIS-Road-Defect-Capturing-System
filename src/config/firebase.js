64b2912dc09880f2768149c1db016856c4991606
LucienChu <lamborkingly@gmail.com>
2020-02-28 09:17:48 -0500

    done with setting up private folder

--- src/config/firebase.js
+++ src/config/firebase.js
@@ -1,7 +1,8 @@
 import * as firebase from 'firebase/app';
 import "firebase/auth";
 import "firebase/storage";
+import {firebaseCredential}  from "../private/credential"
 
-const app = firebase.initializeApp();
+const app = firebase.initializeApp(firebaseCredential);
 
 export default app;
